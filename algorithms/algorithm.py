9aa5064b6975fa3f1167e2332776626ac5022779
htessier <tessier.hug@gmail.com>
2023-11-03 15:49:52 -0400

    Improved implementation and added detailed comments

--- algorithms/algorithm.py
+++ algorithms/algorithm.py
@@ -1,28 +1,40 @@
 from abc import ABC, abstractmethod
 import numpy as np
+from typing import Tuple
 
 
 class Algorithm(ABC):
     @staticmethod
-    def generate_random_qubo_solution(length: int) -> np.ndarray:
+    def generate_random_solution(length: int) -> np.ndarray:
+        """
+        Generates a random vector of bits in {0,1} of a given length.
+
+        :param length: Length of the vector to generate.
+        :return: The generated vector.
+        """
         x = np.zeros(length)
         x[np.random.choice(length, length // 2, replace=False)] = 1.0
         return x
 
     @staticmethod
-    def flip_bit(x: np.ndarray, index: int) -> np.ndarray:
-        y = np.copy(x)
-        if y[index] == 0.:
-            y[index] = 1.
-            return y
-        else:
-            y[index] = 0.
-            return y
+    def compute_energy(x: np.ndarray, qubo: np.ndarray, offset: float) -> float:
+        """
+        Computes the Hamiltonian energy of the QUBO problem.
 
-    @staticmethod
-    def compute_qubo(x: np.ndarray, qubo: np.ndarray, offset: float) -> float:
+        :param x: The solution whose energy to evaluate.
+        :param qubo: The matrix of the coupling coefficients of the QUBO problem.
+        :param offset: Offset value of the energy.
+        :return: The evaluated Hamiltonian energy.
+        """
         return np.dot(x.T, np.dot(qubo, x)) + offset
 
     @abstractmethod
-    def solve_qubo(self, qubo: np.ndarray, offset: float):
+    def __call__(self, qubo: np.ndarray, offset: float) -> Tuple:
+        """
+        Solves a QUBO problem using a given algorithm.
+
+        :param qubo: The matrix of the coupling coefficients of the QUBO problem.
+        :param offset: Offset value of the energy.
+        :return: The solution and a history of the optimization process.
+        """
         pass
--- algorithms/simulated_quantum_annealing.py
+++ algorithms/simulated_quantum_annealing.py
@@ -3,9 +3,19 @@ import numpy as np
 import math
 import random
 import mpmath
+from typing import Tuple
 
 
 class SimulatedQuantumAnnealing(Algorithm):
+    """
+    Based on Accelerating Simulated Quantum Annealing with GPU and Tensor Cores,
+    by Yi-Hua Chung, Cheng-Jhih Shih and Shih-Hao Hung
+    and An Ising computer based on simulated quantum annealing by path integral Monte Carlo method
+    by Takuya Okuyama, Masato Hayashi and Masanao Yamaoka.
+
+    We adapted some calculations to fit QUBO solutions in {0, 1} instead of {-1, 1}.
+    """
+
     def __init__(self,
                  monte_carlo_steps: int,
                  n_trotters: int,
@@ -13,6 +23,15 @@ class SimulatedQuantumAnnealing(Algorithm):
                  end_transverse_field: float = 0.,
                  start_temperature: float = 4.0,
                  end_temperature: float = 1 / 32):
+        """
+        :param monte_carlo_steps: Number of Monte-Carlo steps, i.e. the outermost loop of the algorithm.
+        :param n_trotters: Number of replicas that are optimized in parallel and eventually fused together.
+        :param start_transverse_field: Starting value of the transverse field, that gives the strength of the coupling
+        between trotters.
+        :param end_transverse_field: Final value of the transverse field.
+        :param start_temperature: Starting value of the temperature, that gives the chance of accepting changes.
+        :param end_temperature: Final value of the temperature.
+        """
         self.monte_carlo_steps = monte_carlo_steps
         self.n_trotters = n_trotters
         self.start_transverse_field = start_transverse_field
@@ -20,72 +39,99 @@ class SimulatedQuantumAnnealing(Algorithm):
         self.start_temperature = start_temperature
         self.end_temperature = end_temperature
 
-    def solve_qubo(self, qubo: np.ndarray, offset: float):
+    def _compute_trotters_mean_energy(self, x: np.ndarray, qubo: np.ndarray, offset: float) -> float:
+        # Since, for most of the process, each trotter is more or less independent, we choose to report the mean of
+        # the energy of each trotter
+        energies = []
+        for m in range(self.n_trotters):
+            energies.append(self.compute_energy(x[:, m], qubo, offset))
+        return np.mean(np.array(energies))
+
+    def _update_temperature(self, t: int) -> float:
+        # Linear interpolation; in order to avoid 0 values, we do not take t+1 or monte_carlo_steps-1
+        temperature = self.start_temperature + (
+                (t / self.monte_carlo_steps) * (self.end_temperature - self.start_temperature))
+        return temperature
+
+    def _update_transverse_field(self, t: int) -> float:
+        # Linear interpolation; in order to avoid 0 values, we do not take t+1 or monte_carlo_steps-1
+        transverse_field = self.start_transverse_field + (
+                (t / self.monte_carlo_steps) * (self.end_transverse_field - self.start_transverse_field))
+        return transverse_field
+
+    def _compute_energy_delta(self,
+                              x: np.ndarray,
+                              local_field: np.ndarray,
+                              temperature: float,
+                              transverse_field: float,
+                              i: int,
+                              m: int) -> float:
+        # Weird formula to compute the coupling strength, that increases during training to force trotters to
+        # become the same.
+        replicas_coupling_strength = (temperature / 2) * math.log(
+            mpmath.coth(transverse_field / (self.n_trotters * temperature)))
+        # The coupling depends on the m-1 and m+1 trotters
+        replica_coupling = 0
+        if m + 1 != self.n_trotters:
+            replica_coupling += x[i, m + 1]
+        if m != 0:
+            replica_coupling += x[i, m - 1]
+        # We compute the difference in energy, with a sign that depends on whether the element is set to 0 or 1, and
+        # therefore removed or added.
+        delta_energy = local_field[i, m] - replicas_coupling_strength * replica_coupling
+        flip_direction = 1 if x[i, m] == 0 else -1
+        return delta_energy * flip_direction
+
+    @staticmethod
+    def _fuse_trotters(x: np.ndarray) -> np.ndarray:
+        return (x.T.mean(axis=0) >= 0.5).astype(int)  # Majority judgment over which bits should be 0 or 1
+
+    def __call__(self, qubo: np.ndarray, offset: float) -> Tuple:
         """
-        Based on Accelerating Simulated Quantum Annealing with GPU and Tensor Cores,
-        by Yi-Hua Chung, Cheng-Jhih Shih and Shih-Hao Hung
+        Solves a QUBO problem using Simulated Quantum Annealing (SQA).
+
+        :param qubo: The matrix of the coupling coefficients of the QUBO problem.
+        :param offset: Offset value of the energy.
+        :return: The solution and a history of the optimization process.
         """
+
         length = qubo.shape[0]
-        x = np.stack([self.generate_random_qubo_solution(length) for _ in range(self.n_trotters)]).T
-        local_field = self.initialize_local_field_energy(length, qubo, x)
+
+        # SQA is based on the principle of running n_trotter copies in parallel, that each try to solve the problem
+        # and which are progressively applied a penalty to force them to converge toward a common solution at the end.
+        # These copies are called "Trotters".
+        x = np.stack([self.generate_random_solution(length) for _ in range(self.n_trotters)]).T
+
+        # Since the hamiltonian is purely a sum, the energy delta induced by changing one element of x at a time
+        # can be calculated using only a "local" energy. It is then more efficient to compute said "local energy" field
+        # beforehand and to update it progressively.
+        local_energy_field = np.dot(qubo, x)
+
         history = []
         for t in range(self.monte_carlo_steps):
-            history.append([t, self.compute_qubo((x.T.mean(axis=0) >= 0.5).astype(int), qubo, offset)])
-            temperature = self.update_temperature(t)  # Update temperature
-            transverse_field = self.update_transverse_field(t)  # update transverse_field
+            history.append([t, self._compute_trotters_mean_energy(x, qubo, offset)])
+            temperature = self._update_temperature(t)  # Update temperature
+            transverse_field = self._update_transverse_field(t)  # update transverse_field
             for i in range(length):
                 for m in range(self.n_trotters):
-                    delta_energy = self.compute_energy_delta(x, local_field, temperature, transverse_field, i, m)
+                    delta_energy = self._compute_energy_delta(x, local_energy_field, temperature, transverse_field, i,
+                                                              m)
 
-                    if math.exp(-delta_energy / temperature) > random.random():  # Judge to flip
+                    # Metropolis algorithm: the random number is in [0,1]; if the tested change in x reduces the
+                    # energy, then exp(-delta_energy / temperature) > 1 and the change is accepted; otherwise, we have
+                    # exp(-delta_energy / temperature) in [0,1] too and then, the change is accepted in a probabilistic
+                    # way, with decreasing chances as the energy increase becomes larger.
+                    if math.exp(-delta_energy / temperature) > random.random():
+                        # We flip the i,m element in x
                         if x[i, m] == 1:
                             x[i, m] = 0
                         else:
                             x[i, m] = 1
 
-                        for j in range(length):  # Update local-field energy
-                            # Original paper propose local_field += 2*qubo[i, j]*x[i,m], but that's because it was
-                            # meant for Ising models with spins equal to -1 or 1, whereas for QUBO values are 0 or 1.
-                            if x[i, m] == 1:
-                                local_field[j, m] += qubo[i, j]
-                            else:
-                                local_field[j, m] -= qubo[i, j]
-            # print(x)
-        history.append([self.monte_carlo_steps, self.compute_qubo((x.T.mean(axis=0) >= 0.5).astype(int), qubo, offset)])
-        return (x.T.mean(axis=0) >= 0.5).astype(int), history
+                        # If x[i, m] == 1, then was 0, and we add it; otherwise, now 0 and we remove
+                        flip_direction = 1 if x[i, m] == 1 else -1
+                        local_energy_field[:, m] += (flip_direction * qubo[i, :])
 
-    def compute_energy_delta(self, x, local_field, temperature, transverse_field, i, m):
-        replicas_coupling_strength = (temperature / 2) * math.log(
-            mpmath.coth(transverse_field / (self.n_trotters * temperature)))
-        if m != 0 and m != self.n_trotters - 1:
-            replica_coupling = x[i, m + 1] + x[i, m - 1]
-        elif m == 0:
-            replica_coupling = x[i, m + 1]
-        else:
-            replica_coupling = x[i, m - 1]
-        delta_energy = local_field[i, m] - replicas_coupling_strength * replica_coupling
-        if x[i, m] == 1:
-            return -delta_energy  # Because flipping it would remove the term
-        else:
-            return delta_energy  # Because, if x[i,m] == 0, flipping it would add the term
-
-    def initialize_local_field_energy(self, length, qubo, x):
-        local_field = np.zeros((length, self.n_trotters))
-        for m in range(self.n_trotters):
-            for i in range(length):
-                for j in range(length):
-                    # print(local_field.shape, qubo.shape, x.shape, i, j, m)
-                    local_field[i, m] += qubo[i, j] * x[j, m]
-        return local_field
-
-    def update_temperature(self, t):
-        # Linear interpolation
-        temperature = self.start_temperature + (
-                (t / self.monte_carlo_steps) * (self.end_temperature - self.start_temperature))
-        return temperature
-
-    def update_transverse_field(self, t):
-        # Linear interpolation
-        transverse_field = self.start_transverse_field + (
-                (t / self.monte_carlo_steps) * (self.end_transverse_field - self.start_transverse_field))
-        return transverse_field
+        x = self._fuse_trotters(x)
+        history.append([self.monte_carlo_steps, self.compute_energy(x, qubo, offset)])
+        return x, history
